555162fe3c13803a629f00821b0a7adfe0725862
grh-gitee <3494242908@qq.com>
2022-12-26 16:11:45 +0800

    refactor: 项目构建

---
+++ bin/cli.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+const cli = require('../src/cli')
+
+console.log('Hello World Script-Cli')
+
+cli()
---
+++ src/cli.js
@@ -0,0 +1,4 @@
+module.exports = function cli() {
+  // 打印命令参数
+  console.log(process.argv);
+};
