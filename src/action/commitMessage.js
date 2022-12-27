4c94344761ae1995085140028e47476b17d6648a
grh-gitee <3494242908@qq.com>
2022-12-27 14:52:14 +0800

    [object Object]

--- src/action/commitMessage.js
+++ src/action/commitMessage.js
@@ -9,6 +9,7 @@ module.exports = async function () {
 
 	try {
 		console.log(execSync('git add .').toString())
+		
 		console.log(execSync(`git commit -m  "${commitMessage}"`).toString())
 	} catch (error) {
 		console.log('Commit Message Error')
