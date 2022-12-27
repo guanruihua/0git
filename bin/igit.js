aa7afaf17440f8eb004c1a15b0ca7cff02969aa0
grh-gitee <3494242908@qq.com>
2022-12-27 15:19:51 +0800

    [object Object]

--- bin/igit.js
+++ bin/igit.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 const program = require('commander')
-const { select, input, execSync } = require('../src/hook')
+// const { select, input, execSync } = require('../src/hook')
 const { commitMessage } = require('../src/action')
 const pkg = require('../package.json')
 
 program.version(pkg.version)
+
+const timeString = new Date().toDateString()
 
 program
 	.name('igit')
@@ -12,9 +14,9 @@ program
 	.option('-p, --push', 'Push modification to Repositories')
 	.option('-b, --branch', 'Push Change To Repositories')
 	.action(function (action) {
-		console.log({ action }, new Date().toDateString())
+		console.log({ action })
 		if (action.commit) {
-			commitMessage(`test: ${new Date().toDateString()}`)
+			commitMessage(`test: ${timeString}`)
 		}
 	})
 
