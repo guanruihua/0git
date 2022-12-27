daa3efee27e1038f1300d9d0fe0caf4cb8364689
grh-gitee <3494242908@qq.com>
2022-12-27 16:54:27 +0800

    test: Tue Dec 27 2022

--- src/hook/index.js
+++ src/hook/index.js
@@ -3,8 +3,8 @@ module.exports.select = require('./select')
 
 module.exports.execSync = function (command, config = {}) {
 	const { logCommand = true, logResult = true } = config
-	logCommand && console.log("command", command);
+	logCommand && console.log("Command: ", command);
 	const result = require('child_process').execSync(command).toString()
-	logResult && console.log(result)
+	logResult && result && console.log(result)
 	return result
 }
